cd25c890e77778d257b82d8e40111ed6a7db932e
Alon (PC) <downtown2u@gmail.com>
2020-12-10 20:10:07 +0200

    ✨ Added saving of data between runs (locally)

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import praw
 from praw.models import Subreddit, Submission
 from datetime import datetime
+import os
+import json
 
 
 class PullSubreddit:
@@ -10,11 +12,18 @@ class PullSubreddit:
     _MAX_SUBMISSIONS_PER_REQUEST = 100
     _SAVE_PULLED_DATA_FOR_X_SECONDS = 60 * 60 * 24 * 90  # 90 days
 
-    def __init__(self, subreddit: Subreddit):
+    SAVED_DATA_FOLDER = "data"
+    SAVED_DATA_NAME = "%displayname%.json"  # txt between '%' will be replaced.
+
+    def __init__(self, subreddit: Subreddit,):
         self._sub = subreddit
-        self._already_pulled = list()  # empty list
+
+        self._data_file_path = self._generate_data_filepath(subreddit)
+        self._already_pulled = self._get_data_if_exists()
 
     def pull_submission(self,
+                        save_data=True,
+                        clear_old_data=True,
                         # None: don't care. True: Must contain.
                         nsfw: bool = None,
                         selfpost: bool = None,
@@ -22,7 +31,7 @@ class PullSubreddit:
                         ):
 
         # Make a request for submissions.
-        submissions = self._sub.hot(limit=5, )
+        submissions = self._sub.hot(limit=self._MAX_SUBMISSIONS_PER_REQUEST)
 
         if image:
             # to search for an image you must search for a link posts only!
@@ -45,17 +54,26 @@ class PullSubreddit:
 
             if not self._check_if_pulled(submission):
                 self._mark_pulled(submission)
+
+                if clear_old_data:
+                    self.clear_old_data()
+                if save_data:
+                    self.save_pulled_data()
                 return submission
 
-    def _mark_pulled(self, submission):
-        data = {
-            "id": submission.id,
-            "timestamp": datetime.now().timestamp(),
-        }
+    def save_pulled_data(self):
+        """ Save the pulled sumissions data as a json file inside the special
+        `data` folder. This data then will be used in the next run of the script.
+        """
 
-        self._already_pulled.append(data)
+        # Create the directory if it does not exist yet
+        os.makedirs(self.SAVED_DATA_FOLDER, exist_ok=True)
 
-    def _clear_old_data(self):
+        # Save the json data
+        with open(self._data_file_path, mode='w') as data_file:
+            json.dump(self._already_pulled, data_file, indent=4)
+
+    def clear_old_data(self):
 
         # Save current time to compare to saved ones
         now = datetime.now().timestamp()
@@ -67,5 +85,30 @@ class PullSubreddit:
             if now - data['timestamp'] <= self._SAVE_PULLED_DATA_FOR_X_SECONDS
         ]
 
+    def _mark_pulled(self, submission):
+        data = {
+            "id": submission.id,
+            "timestamp": datetime.now().timestamp(),
+        }
+
+        self._already_pulled.append(data)
+
     def _check_if_pulled(self, submission):
         return any(submission.id == data['id'] for data in self._already_pulled)
+
+    def _generate_data_filepath(self, subreddit: Subreddit):
+        """ Generate the path to the data file of the subreddit. """
+
+        filename = self.SAVED_DATA_NAME.replace(
+            "%displayname%", subreddit.display_name)
+        return os.path.join(self.SAVED_DATA_FOLDER, filename)
+
+    def _get_data_if_exists(self,):
+        """ If data already exists, loads and returns it.
+        Otherwise, returns an empty list. """
+
+        if os.path.exists(self._data_file_path):
+            with open(self._data_file_path) as data_file:
+                return json.load(data_file)
+
+        return list()
