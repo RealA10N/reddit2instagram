eff404e9535b5448b53fd42c096478dfbc7c2a8b
Alon (PC) <downtown2u@gmail.com>
2021-05-17 13:36:19 +0300

    ⚡️ Created basic setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='reddit2instagram',
+    version='0.0.1',
+    author='Alon Krymgand Osovsky',
+    author_email='downtown2u@gmail.com',
+    description='Converting reddit submissions into instagram posts has never been easier.',
+    url='https://github.com/reala10n/reddit2instagram',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    packages=setuptools.find_packages()
+)
