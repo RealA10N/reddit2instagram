1899dd8f9d81cd8215684cb406c722c75de20aa3
Alon (PC) <downtown2u@gmail.com>
2021-05-17 13:58:10 +0300

    🐛 setup.py now installs requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+from os import path
 import setuptools
+
+
+def get_requirements():
+    """ Returns a list of required packages to be installed.
+    Loads the list from the requirements.txt file. """
+
+    this = path.realpath(__file__)
+    here = path.dirname(this)
+    requirements = path.join(here, 'requirements.txt')
+
+    with open(requirements, 'r') as file:
+        return file.read().splitlines()
+
 
 setuptools.setup(
     name='reddit2instagram',
@@ -11,5 +25,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
     ],
     python_requires=">=3.6",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    install_requirements=get_requirements(),
 )
