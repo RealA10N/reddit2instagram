e33a9fc422bb3406da08432e507066646e61a3df
Alon (PC) <downtown2u@gmail.com>
2021-05-22 16:43:50 +0300

    ✨ Added the `TemplateCheckErrorCollection` object

--- reddit2instagram/templates/errors.py
+++ reddit2instagram/templates/errors.py
@@ -1,4 +1,5 @@
 import typing
+from collections import defaultdict
 
 
 class TemplateCheckError:
@@ -72,3 +73,29 @@ class TemplateCheckInvalidDataError(TemplateCheckError):
     def got_str(self,) -> str:
         """ A string that represents the given type """
         return type(self.got).__name__
+
+
+class TemplateCheckErrorCollection:
+    """ An object that collects errors and can display them to the user. """
+
+    def __init__(self):
+        self.by_type = defaultdict(list)
+        self.by_order = list()
+
+    def add_error(self, error: TemplateCheckError):
+        """ Add an error to the collection. """
+
+        self.by_type[type(error)].append(error)
+        self.by_order.append(error)
+
+    def print_errors(self, group_by_type=False):
+        """ Print the collected errors to the user. """
+
+        errors = [
+            error
+            for error in error_group
+            for error_group in self.by_type.values()
+        ] if group_by_type else self.by_order
+
+        for error in errors:
+            print(error.description)
