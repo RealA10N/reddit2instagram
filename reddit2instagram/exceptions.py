91c6efc08e8feec4e1770d2956ec6f49cdb6e706
Alon (PC) <downtown2u@gmail.com>
2021-05-23 22:52:19 +0300

    ✨ Template check now raises errors if check fails

--- reddit2instagram/exceptions.py
+++ reddit2instagram/exceptions.py
@@ -2,5 +2,6 @@ class Reddit2InstagramException(Exception):
     pass
 
 
-class InvalidConfigFileType(Reddit2InstagramException, TypeError):
-    pass
+class InvalidTemplateConfiguration(Reddit2InstagramException, TypeError):
+    """ An exception that is raised when a template isn't configured
+    correctly. """
--- reddit2instagram/templates/basic.py
+++ reddit2instagram/templates/basic.py
@@ -1,27 +1,60 @@
+import typing
+from reddit2instagram.exceptions import InvalidTemplateConfiguration
+from .errors import (
+    TemplateCheckError,
+    TemplateCheckInvalidDataError,
+    TemplateCheckMissingDataError,
+)
+
+
 class Template:
 
-    def __init__(self, *valid_types):
-        self.valid_types = valid_types
+    def __init__(self, *valid_types: typing.Union[type, 'Template']):
+        base_types = list()
+        templates = list()
 
-        assert all(
-            isinstance(cur, (type, Template))
-            for cur in valid_types
+        for type_ in valid_types:
+            if isinstance(type_, type):
+                # If a basic type like `str`, `int`, etc.
+                base_types.append(type_)
+
+            elif isinstance(type_, Template):
+                # If an instance of a Template
+                templates.append(type_)
+
+            else:
+                raise InvalidTemplateConfiguration(
+                    "Accepted templates are object types and other template " +
+                    f"instances, not '{type(type_).__name__}'"
+                )
+
+        # Converting valid type lists to tuples
+        self.base_types = tuple(base_types)
+        self.templates = tuple(templates)
+
+    def check(self,
+              data: typing.Any,
+              path: typing.Tuple[str] = tuple(),
+              ) -> None:
+
+        # Basic type check
+        if isinstance(data, self.base_types):
+            # If one of the accepted basic types, accepts it immediately and
+            # doesn't raise an error.
+            return
+
+        # Template types check
+        for template in self.templates:
+            try: template.check(data)
+            except TemplateCheckError: continue  # If check fails -> tries next template
+            else: return  # If check passed successfully -> exits current check
+
+        # If both checks failed, raise an error
+        raise TemplateCheckInvalidDataError(
+            path=path,
+            expected=self.base_types + self.templates,
+            got=data,
         )
-
-    def check(self, data):
-
-        for valid_type in self.valid_types:
-            if isinstance(valid_type, Template):
-                try:
-                    valid_type.check(data)
-                    return
-                except:
-                    continue
-
-            if isinstance(data, valid_type):
-                return
-
-        assert False
 
 
 class TemplateList(Template):
@@ -30,31 +63,63 @@ class TemplateList(Template):
         super().__init__(list, tuple)
         self.element_template = Template(*valid_types)
 
-    def check(self, data):
-        super().check(data)
-        for element in data:
-            self.element_template.check(element)
+    def check(self,
+              data: typing.Any,
+              path: typing.Tuple[str] = tuple(),
+              ) -> None:
+        # Check if data is a list
+        super().check(data, path)
+
+        # For each element in the list,
+        # check if it follows the element template
+        for index, element in enumerate(data):
+            # append current list index to path
+            cur_path = path + (str(index),)
+            self.element_template.check(element, path=cur_path)
 
 
 class TemplateDict(Template):
 
     def __init__(self, **template):
         super().__init__(dict)
-
         self.template = template
 
-        assert all(
-            isinstance(value, Template)
-            for value in template.values()
+        # Check if all values are template instances
+        try: error_element = next(
+            element
+            for element in template.values()
+            if not isinstance(element, Template)
         )
 
-    def check(self, data):
-        super().check(data)
+        # If found a non template instance, raises an error
+        except StopIteration: pass
+        else:
+            raise InvalidTemplateConfiguration(
+                "Kwargs values should be template instances, " +
+                f"not {type(error_element).__name__}"
+            )
 
-        assert all(
-            key in data
+    def check(self,
+              data: typing.Any,
+              path: typing.Tuple[str] = tuple(),
+              ) -> None:
+        # Check if the data is a dictionary
+        super().check(data, path)
+
+        # Check if all required keys are present in the given data
+        try: missing_key = next(
+            key
             for key in self.template
+            if key not in data
         )
 
+        # If found missing key, raises `MissingData` error
+        except StopIteration: pass
+        else:
+            missing_path = path + (missing_key,)
+            raise TemplateCheckMissingDataError(missing_path)
+
+        # For each key, check for corresponding template
         for key in self.template:
-            self.template[key].check(data[key])
+            cur_path = path + (key,)
+            self.template[key].check(data[key], path=cur_path)
