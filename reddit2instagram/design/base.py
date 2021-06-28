e3dfb1eced84f790a420471c74a171001cc8a519
Alon (PC) <downtown2u@gmail.com>
2021-06-28 21:17:46 +0300

    ✨ The `Design` object now has `generate_from_image` and `..._from_selfpost` methods

--- reddit2instagram/design/base.py
+++ reddit2instagram/design/base.py
@@ -21,7 +21,10 @@ class DesignInfo:
     template: BaseTemplate
 
 
+
+@dataclass(frozen=True)
 class Design(ABC):
+    submission: Submission
 
     @property
     @classmethod
@@ -32,14 +35,32 @@ class Design(ABC):
         appear in the configuration file). """
 
     @abstractmethod
-    def submission_to_image(self,
-                            submission: Submission,
-                            ) -> typing.Optional[Image.Image]:
-        """ A method that recives a reddit submission and converts it into
-        a `PIL` image instance. This method actually defines the design of
-        the Instagram posts, because the image that is returned from this
-        function directly gets uploaded to instagram.
-        If the implementation of this function returns `None` instance of
-        an `Image` instance, `reddit2instagram` will assume that the designer
+    def generate_from_image(self,
+                            img: Image.Image,
+                            ) -> Image.Image:
+        """ Recives a raw image (as it is posted on reddit), and returns the
+        designed image that will be posted onto Instagram.
+
+        This method actually defines the design of the Instagram posts, because
+        the image that is returned from this function directly gets uploaded to
+        instagram. If the implementation of this function returns `None` instance
+        of an `Image` instance, `reddit2instagram` will assume that the designer
         failed to design an image for the given submission, and a different
-        submission will be picked instead. """
+        submission will be picked instead.
+
+        When implementing this method, use the given image and decorate it. The
+        submission instance is provided using `self.submission` if it is needed
+        (for example, to give credit to the original redditor that posted the image
+        and to add his name as a text on top of the image). """
+
+    @abstractmethod
+    def generate_from_selfpost(self,) -> Image.Image:
+        """ This method assumes that the submission is a selfpost (no image, only
+        text), and generates the final image that will be uploaded to Instagram.
+
+        The text, author and  additional information about the submission can be
+        accessed using the `self.submission` property, which contains an
+        `praw.modules.Submission` instance.  """
+
+    def __repr__(self):
+        return f'Design(name={self.info.name!r}, submission={self.submission.id!r})'
